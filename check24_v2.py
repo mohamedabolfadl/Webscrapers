c0220845311d53541cbe1e4b047adf6943ce0e8a
mohamedabolfadl <mabolfadl@gmail.com>
2017-05-26 22:34:20 +0200

    Check24
    Scraping check24

---
+++ check24_v2.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 18:05:08 2016
+
+@author: Moh2
+"""
+
+import re
+import urlparse
+import urllib2
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time 
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+#, sys
+#import subprocess
+import csv
+# save everything even without recommendations and place "-" in no recommendation field
+# Save a structure with hotel id and price... so that we can plot vs time
+
+
+npages = 8
+pc = 1
+urlss = []
+nummber_recs = []
+recommendation_values = []
+hotel_names = []
+prices_hotels =[]
+durations = []
+airports = []
+dep_dates = []
+ret_dates = []
+isOffers = []
+hotel_ids = []
+nonrated_prices = []
+meals = []
+strs = []
+#baselink = "https://urlaub.check24.de/suche/hotel?regionId=764&extendedSearch=1&sorting=rating&order=desc&regionSort=topregion&regionSortOrder=asc&hotelCategory=&airport=BLL,BRE,BSL,CGN,CSO,DRS,DTM,DUS,EIN,ENS,ERF,FDH,FKB,FMM,FMO,FRA,GWT,HAJ,HAM,HDF,HHN,INN,KSF,LBC,LEJ,LGG,LUX,MST,MUC,NRN,NUE,PAD,PRG,RLG,SCN,STR,SXB,SXF,SZG,SZW,TXL,ZQW,ZRH&departureDate=2016-11-01&returnDate=2017-02-28&travelDuration=1w&adult=2&recommendation=-&page="
+baselink = "https://urlaub.check24.de/suche/hotel?regionId=764&extendedSearch=1&regionSort=topregion&regionSortOrder=asc&hotelCategory=&airport=BLL,BRE,BSL,CGN,CSO,DRS,DTM,DUS,EIN,ENS,ERF,FDH,FKB,FMM,FMO,FRA,GWT,HAJ,HAM,HDF,HHN,INN,KSF,LBC,LEJ,LGG,LUX,MST,MUC,NRN,NUE,PAD,PRG,RLG,SCN,STR,SXB,SXF,SZG,SZW,TXL,ZQW,ZRH&adult=2&departureDate=2016-11-01&returnDate=2017-02-28&travelDuration=1w&page="
+while pc<=npages :
+    print "page:"+str(pc)
+    link = baselink+str(pc)
+    pc = pc+1    
+    page = urllib2.urlopen(link)
+    soup_packtpage = BeautifulSoup(page)
+    
+    
+    ban=[]
+    i=1
+    #getting urls
+    print "picking urls..."
+    all_offers = soup_packtpage.find_all("a",class_="hotel-list-offer-link no-spin")
+    for offer in all_offers:
+        st = time.time()        
+        print "url no.="+str(i)
+        time.sleep(0.75)
+        div = offer.findParent("div",class_="offer-cnt")
+        rev = div.findAll("span",class_="reviews")
+        if rev:
+            tmp = offer["href"]
+            tmp=tmp.encode('utf-8')
+            urlss.append(tmp)
+            k=tmp.find("hotelId=")
+            hotel_ids.append(tmp[k+8:len(tmp)])
+            trip_page = urllib2.urlopen(tmp)
+            soup = BeautifulSoup(trip_page)
+
+            ret = soup.find("li",class_="list-icon-ele")
+            tmp = ret.text
+            tmp = tmp.encode('utf-8')
+            tmp = tmp[2:len(tmp)-1]
+            meals.append(tmp)
+            
+            offer_box = soup.find("div",class_="offer-box")
+            duration = offer_box.find_next("span")
+            tmp =        duration.string
+            tmp=tmp.encode('utf-8')
+            k=tmp.find(" ")
+            tmp=tmp[0:k]
+            durations.append(tmp)
+
+            tmp = duration.find_next("span").string
+            tmp = tmp.encode('utf-8')
+            airports.append(tmp)
+
+            dep = soup.find("span",class_="flight-date-ele")
+            tmp = dep.string
+            tmp = tmp.encode('utf-8')
+            tmp = tmp[4:12]
+            dep_dates.append(tmp)
+
+            ret = dep.find_next("span",class_="flight-date-ele")
+            tmp = ret.string
+            tmp = tmp.encode('utf-8')
+            tmp = tmp[4:12]
+            ret_dates.append(tmp)
+
+
+
+            isOffer = div.find("span",class_="stroke-price-ele")
+            if isOffer:
+                isOffers.append(1)
+            else:
+                isOffers.append(0)
+             
+                
+        else:
+            ban.append(i)
+        i = i+1
+        en = time.time()
+        elap = en-st
+        print "Rest time ="+str((((npages+1-pc)*25*elap)+elap*(25-i))/60)+" minutes"
+    
+    print "picking number of recommendations..."
+    kw = "B"
+    # Getting number of recommendations
+    all_nrecs = soup_packtpage.find_all("span",class_="reviews")
+    for nrec in all_nrecs:
+        tmp = nrec.string.encode('utf-8')
+        k=tmp.find(kw)
+        tmp=tmp[1:k-1]
+        nummber_recs.append(int(tmp))
+    print "picking recommendations..."    
+    kw = "%"
+    # Getting recommendations
+    all_recs = soup_packtpage.find_all("span",class_="recommendation-percent")
+    for rec in all_recs:
+        tmp=rec.string.encode('utf-8')
+        k=tmp.find(kw)
+        tmp=tmp[0:k]
+        recommendation_values.append(int(tmp))
+    print "picking titles..."
+    # Getting title
+    i=1
+    all_titles = soup_packtpage.find_all("div",class_="offer-header")
+    for title in all_titles:
+        #div = title.findParent("div",class_="offer-cnt")
+        #rev = div.findAll("span",class_="reviews")
+        #if rev:
+         if i not in ban:
+            tmp  = title.a.string.encode('utf-8')
+            tmp = tmp[1:len(tmp)]
+            hotel_names.append(tmp)
+         i=i+1
+    
+    kw = " "
+    i=1
+    print "picking prices..."
+    # Getting prices
+    all_prices = soup_packtpage.find_all("span",class_="price")
+    for price in all_prices:
+        tmp = price.string.encode('utf-8')
+        tmp=tmp.replace('.','')
+        k=tmp.find(kw)
+        tmp=tmp[0:k]   
+        #div = price.findParent("div",class_="offer-cnt")
+        #rev = div.findAll("span",class_="reviews")
+        #if rev:
+        if i not in ban:
+            prices_hotels.append(int(tmp))
+        else:
+            nonrated_prices.append(int(tmp))
+        i=i+1
+    
+    
+    #getting stars
+    r = re.compile(r'hotel\-category\-b')
+    i=1
+    all_stars = soup_packtpage.find_all("span",class_=r)
+    #print all_stars
+    for star in all_stars:
+        if i not in ban:
+            tmp = star['class']
+            tmp = tmp[1]
+            tmp = tmp [len(tmp)-2:len(tmp)]
+            tmp = tmp.encode('utf-8')
+            strs.append(tmp)
+        i=i+1
+        
+        
+  
+    
+    
+    
+    
+    
+    time.sleep(1)
+print "plotting..."
+
+homedir = "C:/Users/Moh2/Desktop/scraping/dubai"
+os.chdir(homedir)
+destination = "Dubai_"+time.strftime("%d-%m-%Y")+"_"+time.strftime("%I-%M-%S")
+os.mkdir(destination)
+
+
+
+csvFile = open(homedir+"/"+destination+"/"+destination+".csv","w+")
+try:
+    writer =csv.writer(csvFile)
+    writer.writerow(('Hotel ID','Price','Airport','Dep','Arr','Duration','Recommendation','Number of recommendations','Stars','Meal','Name'))
+    for ji in range(len(prices_hotels)):
+        writer.writerow(( hotel_ids[ji] ,prices_hotels[ji] , airports[ji] , dep_dates[ji] , ret_dates[ji] , durations[ji] , recommendation_values[ji] ,nummber_recs[ji] , strs[ji],meals[ji], hotel_names[ji] ))
+finally:
+    csvFile.close()
+    
+    
+    
+fig = plt.figure()
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 8,
+        }
+fontM = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 8,
+        }
+        
+     
+for ci in range(0,len(durations)):
+    tmp = airports[ci]
+    if len(tmp)==8 and tmp[0]=='M':
+        font_sel = fontM
+    else:
+        font_sel = font
+    
+    if strs[ci][0]=='5': # 5 stars
+        mark_code='r'
+    if strs[ci][0]=='4': # 4 stars
+        mark_code='g'
+    if strs[ci][0]=='3': # 3 stars
+        mark_code='b'
+    if strs[ci][0]=='2': # 2 stars
+        mark_code='m'
+    if strs[ci][0]=='1': # 1 stars
+        mark_code='k'
+    
+    
+    if meals[ci][0]=='n': # Only sleep
+        mark_code=mark_code+'o'
+    if meals[ci][0]=='F': # Breakfast
+        mark_code=mark_code+'s'
+    if meals[ci][0]=='H': # Half board
+        mark_code=mark_code+'^'
+    if meals[ci][0]=='V': # Full board
+        mark_code=mark_code+'d'
+    if meals[ci][0]=='A': # Full board
+        mark_code=mark_code+'*'
+        
+        
+
+        
+    plt.plot(nummber_recs[ci],prices_hotels[ci],mark_code)
+    plt.text(0.5+nummber_recs[ci],-5+prices_hotels[ci], str(ci+1)+"|"+str(recommendation_values[ci]), fontdict=font_sel)
+
+
+
+    
+plt.plot(np.zeros(len(nonrated_prices)),nonrated_prices,'yo')
+plt.grid()
+plt.xlabel("Number of recommendations")
+plt.ylabel("Price")
+plt.axhline(500, 0,500, 300)
+plt.ylim(100,2000)
+plt.show()
+
+fig.savefig(destination+'/dubai_fig.eps')
+fig.savefig(destination+'/dubai_fig.png')
+
+plt.xlim(0,100)
+
+fig.savefig(destination+'/dubai_fig_z.eps')
+fig.savefig(destination+'/dubai_fig_z.png')
+
+print "filling latex..."
+
+
+
+
+tfile = open(destination+'/'+str(destination)+".tex", "w")
+tfile.write("\n")
+tfile.write("\documentclass{article}")
+tfile.write("\n")
+tfile.write("\usepackage[a4paper, total={8in, 10in}]{geometry}")
+tfile.write("\n")
+tfile.write("\usepackage{color}")
+tfile.write("\n")
+tfile.write("\usepackage{mathrsfs,amsfonts,graphicx,color, amsmath, setspace, epstopdf,caption,slashbox,hyperref}")
+tfile.write("\n")
+tfile.write("\setlength\parindent{0pt}")
+tfile.write("\n")
+tfile.write("\\title{Dubai}")
+tfile.write("\n")
+tfile.write("\\begin{document}")
+tfile.write("\n")
+tfile.write("\maketitle")
+tfile.write("\n")
+oi = 0
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig.eps} ")
+tfile.write("\n")
+tfile.write("\caption{price vs popularity}")
+tfile.write("\n")
+tfile.write("\label{F:pvp} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_z.eps} ")
+tfile.write("\n")
+tfile.write("\caption{price vs popularity zoom}")
+tfile.write("\n")
+tfile.write("\label{F:pvpz} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+
+while oi<len(urlss):
+    tfile.write("\section{"+hotel_names[oi].replace("&","\&")+"-{\color{red}"+str(prices_hotels[oi])+"}-"+"("+str(hotel_ids[oi])+")"+"}")
+    tfile.write("\n")
+    tfile.write("Airport:"+str(airports[oi])+"-")
+    tfile.write("\n")    
+    tfile.write("Dep date:"+str(dep_dates[oi])+"-")
+    tfile.write("\n")
+    tfile.write("Ret date:"+str(ret_dates[oi])+"-")
+    tfile.write("\n")
+    tfile.write("Recommendations:"+str(recommendation_values[oi])+"\% -")
+    tfile.write("\n")
+    tfile.write("Number of Rec:"+str(nummber_recs[oi])+"-")
+    tfile.write("\n")
+    tfile.write("Duration:"+str(durations[oi])+"-")
+    tfile.write("\n")
+    tfile.write("\n")
+    tfile.write("\n")
+    
+    
+    
+    oi=oi+1
+
+tfile.write("\n")    
+tfile.write("\end{document}")    
+tfile.close()  
+print "Compiling latex..."
+
+time.sleep(1)
+os.chdir(destination)
+comnd = "pdflatex.exe -synctex=1 -interaction=nonstopmode "+destination+".tex"
+os.system(comnd)
+os.chdir(homedir)
+print "Done..."
+#subprocess.call(comnd)
+
---
+++ check24_v2_light_minHalbPension.py
@@ -0,0 +1,457 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 18:05:08 2016
+
+@author: Moh2
+"""
+
+import re
+import urlparse
+import urllib2
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time 
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+#, sys
+#import subprocess
+import csv
+# save everything even without recommendations and place "-" in no recommendation field
+# Save a structure with hotel id and price... so that we can plot vs time
+
+
+npages = 8
+pc = 1
+urlss = []
+nummber_recs = []
+recommendation_values = []
+hotel_names = []
+prices_hotels =[]
+durations = []
+airports = []
+dep_dates = []
+ret_dates = []
+isOffers = []
+hotel_ids = []
+nonrated_prices = []
+meals = []
+strs = []
+#baselink = "https://urlaub.check24.de/suche/hotel?regionId=764&extendedSearch=1&sorting=rating&order=desc&regionSort=topregion&regionSortOrder=asc&hotelCategory=&airport=BLL,BRE,BSL,CGN,CSO,DRS,DTM,DUS,EIN,ENS,ERF,FDH,FKB,FMM,FMO,FRA,GWT,HAJ,HAM,HDF,HHN,INN,KSF,LBC,LEJ,LGG,LUX,MST,MUC,NRN,NUE,PAD,PRG,RLG,SCN,STR,SXB,SXF,SZG,SZW,TXL,ZQW,ZRH&departureDate=2016-11-01&returnDate=2017-02-28&travelDuration=1w&adult=2&recommendation=-&page="
+baselink = "https://urlaub.check24.de/suche/hotel?airport=BLL,BRE,BSL,CGN,CSO,DRS,DTM,DUS,EIN,ENS,ERF,FDH,FKB,FMM,FMO,FRA,GWT,HAJ,HAM,HDF,HHN,INN,KSF,LBC,LEJ,LGG,LUX,MST,MUC,NRN,NUE,PAD,PRG,RLG,SCN,STR,SXB,SXF,SZG,SZW,TXL,ZQW,ZRH&adult=2&departureDate=2016-11-01&returnDate=2017-02-28&travelDuration=1w&catering=halfboard&regionId=764&recommendation=-&sorting=categoryDistribution&order=asc&regionSort=topregion&regionSortOrder=asc&extendedSearch=1&oceanView=0&page="
+while pc<=npages :
+    print "page:"+str(pc)
+    link = baselink+str(pc)
+    pc = pc+1    
+    page = urllib2.urlopen(link)
+    soup_packtpage = BeautifulSoup(page)
+    
+    all_offers = soup_packtpage.find_all("div",class_="offer-cnt")
+    for offer in all_offers:
+        li=offer.find("a",class_="hotel-list-offer-link no-spin")
+        if li:
+            url = li["href"]
+            urlss.append(url.encode('utf-8'))
+        
+        name = offer.find("div",class_="offer-header")
+        if name:        
+            tmp  = name.a.string.encode('utf-8')
+            tmp = tmp[1:len(tmp)]
+            hotel_names.append(tmp)  
+            
+        price = offer.find("span",class_="price")
+        if price:        
+            tmp = price.string.encode('utf-8')
+            tmp=tmp.replace('.','')
+            kw = " "
+            k=tmp.find(kw)
+            tmp=tmp[0:k]   
+            prices_hotels.append(int(tmp))
+
+        duration = offer.find("div",class_="duration")
+        if duration:        
+            tmp =        duration.string
+            tmp=tmp.encode('utf-8')
+            k=tmp.find(" ")
+            tmp=tmp[0:k]
+            durations.append(tmp)
+            
+            r = re.compile(r'hotel\-category\-b')
+            star = offer.find("span",class_=r)
+        if star:
+            tmp = star['class']
+            tmp = tmp[1]
+            tmp = tmp [len(tmp)-2:len(tmp)]
+            tmp = tmp.encode('utf-8')
+            strs.append(tmp)
+        
+        kw = "B"
+        nrecs = offer.find("span",class_="reviews")
+        if nrecs:
+            tmp = nrecs.string.encode('utf-8')
+            k=tmp.find(kw)
+            tmp=tmp[1:k-1]
+            nummber_recs.append(int(tmp))
+        else:
+            nummber_recs.append(int("0"))
+        
+        kw = "%"
+        recs = offer.find("span",class_="recommendation-percent")
+        if recs:
+            tmp=recs.string.encode('utf-8')
+            k=tmp.find(kw)
+            tmp=tmp[0:k]
+            recommendation_values.append(int(tmp))
+        else:
+            recommendation_values.append(int("0"))
+        
+            
+
+    time.sleep(1)
+  
+print "plotting..."
+
+homedir = "C:/Users/Moh2/Desktop/scraping/dubai"
+os.chdir(homedir)
+destination = "Dubai_light_"+time.strftime("%d-%m-%Y")+"_"+time.strftime("%I-%M-%S")+"_minHalbPension"
+os.mkdir(destination)
+
+
+
+csvFile = open(homedir+"/"+destination+"/"+destination+".csv","w+")
+try:
+    writer =csv.writer(csvFile)
+    writer.writerow(('Price','Duration','Recommendation','Number of recommendations','Stars','Name'))
+    for ji in range(len(prices_hotels)):
+        writer.writerow((prices_hotels[ji] , durations[ji] , recommendation_values[ji] ,nummber_recs[ji] , strs[ji], hotel_names[ji] ))
+finally:
+    csvFile.close()
+    
+    
+    
+fig = plt.figure()
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 8,
+        }
+fontM = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 8,
+        }
+        
+font_sel = font     
+for ci in range(0,len(durations)):
+
+    mark_code=''
+    if strs[ci][0]=='5': # 5 stars
+        mark_code='r'
+    if strs[ci][0]=='4': # 4 stars
+        mark_code='g'
+    if strs[ci][0]=='3': # 3 stars
+        mark_code='b'
+    if strs[ci][0]=='2': # 2 stars
+        mark_code='m'
+    if strs[ci][0]=='1': # 1 stars
+        mark_code='k'
+    
+    if recommendation_values[ci]>=95 :
+        mark_code=mark_code+"*"
+        
+    if recommendation_values[ci]>=90 and recommendation_values[ci]<95 :
+        mark_code=mark_code+"d"
+
+    if recommendation_values[ci]>=85 and recommendation_values[ci]<90 :
+        mark_code=mark_code+"s"
+
+    if recommendation_values[ci]>=80 and recommendation_values[ci]<85 :
+        mark_code=mark_code+"o"
+        
+    if recommendation_values[ci]<80 :
+        mark_code=mark_code+"x"
+
+    
+
+        
+        
+
+        
+    plt.plot(nummber_recs[ci],prices_hotels[ci],mark_code)
+    plt.text(0.5+nummber_recs[ci],-5+prices_hotels[ci], str(ci+1), fontdict=font_sel)
+
+
+
+    
+plt.grid()
+plt.xlabel("Number of recommendations")
+plt.ylabel("Price")
+plt.axhline(500, 0,500, 300)
+plt.ylim(100,2000)
+plt.show()
+
+fig.savefig(destination+'/dubai_fig.eps')
+fig.savefig(destination+'/dubai_fig.png')
+plt.ylim(400,1100)
+plt.xlim(0,100)
+
+fig.savefig(destination+'/dubai_fig_z.eps')
+fig.savefig(destination+'/dubai_fig_z.png')
+
+    
+    
+fig = plt.figure()
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 8,
+        }
+fontM = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 8,
+        }
+        
+font_sel = font     
+for ci in range(0,len(durations)):
+
+    mark_code=''
+    if strs[ci][0]=='5': # 5 stars
+        mark_code='r'
+
+    
+        if recommendation_values[ci]>=95 :
+            mark_code=mark_code+"*"
+            
+        if recommendation_values[ci]>=90 and recommendation_values[ci]<95 :
+            mark_code=mark_code+"d"
+    
+        if recommendation_values[ci]>=85 and recommendation_values[ci]<90 :
+            mark_code=mark_code+"s"
+    
+        if recommendation_values[ci]>=80 and recommendation_values[ci]<85 :
+            mark_code=mark_code+"o"
+            
+        if recommendation_values[ci]<80 :
+            mark_code=mark_code+"x"
+    
+        
+    
+            
+            
+    
+            
+        plt.plot(nummber_recs[ci],prices_hotels[ci],mark_code)
+        plt.text(0.5+nummber_recs[ci],-5+prices_hotels[ci], str(ci+1), fontdict=font_sel)
+
+
+
+    
+plt.grid()
+plt.xlabel("Number of recommendations")
+plt.ylabel("Price")
+plt.axhline(500, 0,500, 300)
+plt.ylim(100,2000)
+plt.show()
+
+fig.savefig(destination+'/dubai_fig_5.eps')
+fig.savefig(destination+'/dubai_fig_5.png')
+plt.ylim(400,1100)
+plt.xlim(0,100)
+
+fig.savefig(destination+'/dubai_fig_z_5.eps')
+fig.savefig(destination+'/dubai_fig_z_5.png')
+
+
+
+
+
+    
+    
+    
+fig = plt.figure()
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 8,
+        }
+fontM = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 8,
+        }
+        
+font_sel = font     
+for ci in range(0,len(durations)):
+
+    mark_code=''
+    if strs[ci][0]=='5': # 5 stars
+        mark_code='r'
+    if strs[ci][0]=='4': # 4 stars
+        mark_code='g'
+
+    
+        if recommendation_values[ci]>=95 :
+            mark_code=mark_code+"*"
+            
+        if recommendation_values[ci]>=90 and recommendation_values[ci]<95 :
+            mark_code=mark_code+"d"
+    
+        if recommendation_values[ci]>=85 and recommendation_values[ci]<90 :
+            mark_code=mark_code+"s"
+    
+        if recommendation_values[ci]>=80 and recommendation_values[ci]<85 :
+            mark_code=mark_code+"o"
+            
+        if recommendation_values[ci]<80 :
+            mark_code=mark_code+"x"
+    
+        
+    
+            
+            
+    
+            
+        plt.plot(nummber_recs[ci],prices_hotels[ci],mark_code)
+        plt.text(0.5+nummber_recs[ci],-5+prices_hotels[ci], str(ci+1), fontdict=font_sel)
+
+
+
+    
+plt.grid()
+plt.xlabel("Number of recommendations")
+plt.ylabel("Price")
+plt.axhline(500, 0,500, 300)
+plt.ylim(100,2000)
+plt.show()
+
+fig.savefig(destination+'/dubai_fig_4.eps')
+fig.savefig(destination+'/dubai_fig_4.png')
+plt.ylim(400,1100)
+plt.xlim(0,100)
+
+fig.savefig(destination+'/dubai_fig_z_4.eps')
+fig.savefig(destination+'/dubai_fig_z_4.png')
+
+
+print "filling latex..."
+
+
+
+
+tfile = open(destination+'/'+str(destination)+".tex", "w")
+tfile.write("\n")
+tfile.write("\documentclass{article}")
+tfile.write("\n")
+tfile.write("\usepackage[a4paper, total={8in, 10in}]{geometry}")
+tfile.write("\n")
+tfile.write("\usepackage{color}")
+tfile.write("\n")
+tfile.write("\usepackage{mathrsfs,amsfonts,graphicx,color, amsmath, setspace, epstopdf,caption,slashbox,hyperref}")
+tfile.write("\n")
+tfile.write("\setlength\parindent{0pt}")
+tfile.write("\n")
+tfile.write("\\title{Dubai}")
+tfile.write("\n")
+tfile.write("\\begin{document}")
+tfile.write("\n")
+tfile.write("\maketitle")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig.eps} ")
+tfile.write("\n")
+tfile.write("\caption{price vs popularity}")
+tfile.write("\n")
+tfile.write("\label{F:pvp} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_z.eps} ")
+tfile.write("\n")
+tfile.write("\caption{price vs popularity zoom}")
+tfile.write("\n")
+tfile.write("\label{F:pvpz} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_5.eps} ")
+tfile.write("\n")
+tfile.write("\caption{5 star price vs popularity}")
+tfile.write("\n")
+tfile.write("\label{F:pvp} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_z_5.eps} ")
+tfile.write("\n")
+tfile.write("\caption{5 star price vs popularity zoom}")
+tfile.write("\n")
+tfile.write("\label{F:pvpz} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_4.eps} ")
+tfile.write("\n")
+tfile.write("\caption{4 star price vs popularity}")
+tfile.write("\n")
+tfile.write("\label{F:pvp} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+tfile.write("\\begin{figure}[htp!] \centering ")
+tfile.write("\n")
+tfile.write("\includegraphics[keepaspectratio=true, width=210mm]{dubai_fig_z_4.eps} ")
+tfile.write("\n")
+tfile.write("\caption{4 star price vs popularity zoom}")
+tfile.write("\n")
+tfile.write("\label{F:pvpz} ")
+tfile.write("\n")
+tfile.write("\end{figure}")
+tfile.write("\n")
+
+oi = 0
+while oi<len(hotel_names):
+    tfile.write("\section{\href{"+urlss[oi]+"}{"+hotel_names[oi].replace("&","\&")+"}-{\color{red}"+str(prices_hotels[oi])+"}-"+strs[oi][0]+"."+strs[oi][1]+" stars}")
+    tfile.write("\n")
+    tfile.write("Recommendations:"+str(recommendation_values[oi])+"\% -")
+    tfile.write("\n")
+    tfile.write("Number of Rec:"+str(nummber_recs[oi])+"-")
+    tfile.write("\n")
+    tfile.write("Duration:"+str(durations[oi])+"-")
+    tfile.write("\n")
+    tfile.write("\n")
+    tfile.write("\n")
+    
+    
+    
+    oi=oi+1
+
+tfile.write("\n")    
+tfile.write("\end{document}")    
+tfile.close()  
+print "Compiling latex..."
+
+time.sleep(1)
+os.chdir(destination)
+comnd = "pdflatex.exe -synctex=1 -interaction=nonstopmode "+destination+".tex"
+os.system(comnd)
+os.chdir(homedir)
+print "Done..."
+#subprocess.call(comnd)
+
